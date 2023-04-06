feb318822d79818cfc8f8fa1ad52ba6c80478331
Orsola Maria Borrini <orsolaborrini@gmail.com>
2023-04-06 17:53:15 +0200

    Update sparql_viz.js

--- assets/js/sparql_viz.js
+++ assets/js/sparql_viz.js
@@ -10,7 +10,7 @@ function generateData(count) {
   var row = 1;
   var col = 1;
   var data = [];
-  for(var i = 0; i < count; i++) {
+  for (var i = 0; i < count; i++) {
     data.push({
       x: col + "",
       y: row + ""
@@ -26,7 +26,7 @@ function generateData(count) {
 
 function generateCategories(count) {
   var data = [];
-  for(var i = 0; i < count; i++) {
+  for (var i = 0; i < count; i++) {
     data.push({
       cat: (i + 1) + ""
     });
@@ -131,8 +131,8 @@ function makeSeries(name, yAxis, data, color, path) {
     strokeOpacity: 0
   });
 
-  
-  series.bullets.push(function(root) {
+
+  series.bullets.push(function (root) {
     return am5.Bullet.new(root, {
       locationX: 0.5,
       locationY: 0.5,
@@ -145,15 +145,15 @@ function makeSeries(name, yAxis, data, color, path) {
       })
     });
   });
-  
+
   series.data.setAll(data);
-  
+
   series.appear();
   return series;
 }
 
 var femaleColor = am5.color(0xb15859); /* b15859 */ /* f25f5c */
-var maleColor = am5.color(0x3e8488); /* 57b5ba */ /* 247ba0 */ 
+var maleColor = am5.color(0x3e8488); /* 57b5ba */ /* 247ba0 */
 var placeholderColor = am5.color(0x999999);
 
 var maleIcon = "M 25.1 10.7 c 2.1 0 3.7 -1.7 3.7 -3.7 c 0 -2.1 -1.7 -3.7 -3.7 -3.7 c -2.1 0 -3.7 1.7 -3.7 3.7 C 21.4 9 23 10.7 25.1 10.7 z M 28.8 11.5 H 25.1 h -3.7 c -2.8 0 -4.7 2.5 -4.7 4.8 V 27.7 c 0 2.2 3.1 2.2 3.1 0 V 17.2 h 0.6 v 28.6 c 0 3 4.2 2.9 4.3 0 V 29.3 h 0.7 h 0.1 v 16.5 c 0.2 3.1 4.3 2.8 4.3 0 V 17.2 h 0.5 v 10.5 c 0 2.2 3.2 2.2 3.2 0 V 16.3 C 33.5 14 31.6 11.5 28.8 11.5 z";
@@ -239,29 +239,6 @@ series.appear(1000, 100);
 /////////////////////////////// Bubble chart MG2
 ////////////////////////////////////////////////////////////////////////////////
 
-/* // read local JSON file in javascript
-data = fetch("data/sparql/mg2.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
- */
-
-var arr = null;
-$.ajax({
-    'async': false,
-    'global': false,
-    'url': "/data.json",
-    'dataType': "json",
-    'success': function (data) {
-        arr = data;
-    }
-});
-
-console.log(arr)
-
 // Create root element
 // https://www.amcharts.com/docs/v5/getting-started/#Root_element
 var root = am5.Root.new("SPARQLchartMG2");
@@ -278,8 +255,8 @@ var chart = root.container.children.push(am5xy.XYChart.new(root, {
   panX: true,
   panY: true,
   wheelY: "zoomXY",
-  pinchZoomX:true,
-  pinchZoomY:true
+  pinchZoomX: true,
+  pinchZoomY: true
 }));
 
 chart.get("colors").set("step", 2);
@@ -314,7 +291,7 @@ var series0 = chart.series.push(am5xy.LineSeries.new(root, {
 // Add bullet
 // https://www.amcharts.com/docs/v5/charts/xy-chart/series/#Bullets
 var circleTemplate = am5.Template.new({});
-series0.bullets.push(function() {
+series0.bullets.push(function () {
   var graphics = am5.Circle.new(root, {
     fill: series0.get("fill"),
   }, circleTemplate);
@@ -352,6 +329,215 @@ chart.set("scrollbarX", am5.Scrollbar.new(root, {
 chart.set("scrollbarY", am5.Scrollbar.new(root, {
   orientation: "vertical"
 }));
+
+
+var data = [
+  {
+    "ProductionCosts": 140000000.0,
+    "BoxOffice": 305413918.0,
+    "gaze_score": 30.2582532386
+  },
+  {
+    "ProductionCosts": 90000000.0,
+    "BoxOffice": 250690539.0,
+    "gaze_score": 51.66
+  },
+  {
+    "ProductionCosts": 113000000.0,
+    "BoxOffice": 380270577.0,
+    "gaze_score": 54.8682130082
+  },
+  {
+    "ProductionCosts": 54000000.0,
+    "BoxOffice": 108185706.0,
+    "gaze_score": 32.1291752335
+  },
+  {
+    "ProductionCosts": 200000000.0,
+    "BoxOffice": 659363944.0,
+    "gaze_score": 51.6625
+  },
+  {
+    "ProductionCosts": 11000000.0,
+    "BoxOffice": 460998507.0,
+    "gaze_score": 51.66
+  },
+  {
+    "ProductionCosts": 125000000.0,
+    "BoxOffice": 215409889.0,
+    "gaze_score": 71.2934042348
+  },
+  {
+    "ProductionCosts": 225000000.0,
+    "BoxOffice": 291045518.0,
+    "gaze_score": 35.1380082444
+  },
+  {
+    "ProductionCosts": 10500000.0,
+    "BoxOffice": 435110554.0,
+    "gaze_score": 0.0
+  },
+  {
+    "ProductionCosts": 250000000.0,
+    "BoxOffice": 381447587.0,
+    "gaze_score": 33.9678714308
+  },
+  {
+    "ProductionCosts": 15000000.0,
+    "BoxOffice": 45306425.0,
+    "gaze_score": 65.0
+  },
+  {
+    "ProductionCosts": 139000000.0,
+    "BoxOffice": 407022860.0,
+    "gaze_score": 60.7950256305
+  },
+  {
+    "ProductionCosts": 55000000.0,
+    "BoxOffice": 134478449.0,
+    "gaze_score": 55.8490738524
+  },
+  {
+    "ProductionCosts": 100000000.0,
+    "BoxOffice": 184069126.0,
+    "gaze_score": 51.66
+  },
+  {
+    "ProductionCosts": 80000000.0,
+    "BoxOffice": 162924631.0,
+    "gaze_score": 61.1606158499
+  },
+  {
+    "ProductionCosts": 1400000.0,
+    "BoxOffice": 8400000.0,
+    "gaze_score": 21.0764244322
+  },
+  {
+    "ProductionCosts": 200000000.0,
+    "BoxOffice": 373585825.0,
+    "gaze_score": 55.0795570414
+  },
+  {
+    "ProductionCosts": 9000000.0,
+    "BoxOffice": 260758300.0,
+    "gaze_score": 25.0
+  },
+  {
+    "ProductionCosts": 220000000.0,
+    "BoxOffice": 623357910.0,
+    "gaze_score": 54.8682130082
+  },
+  {
+    "ProductionCosts": null,
+    "BoxOffice": 319246193.0,
+    "gaze_score": 0.0
+  },
+  {
+    "ProductionCosts": 32500000.0,
+    "BoxOffice": 309306177.0,
+    "gaze_score": 51.66
+  },
+  {
+    "ProductionCosts": 18000000.0,
+    "BoxOffice": 292753960.0,
+    "gaze_score": 65.0
+  },
+  {
+    "ProductionCosts": 102000000.0,
+    "BoxOffice": 205881154.0,
+    "gaze_score": 42.2933556206
+  },
+  {
+    "ProductionCosts": 63000000.0,
+    "BoxOffice": 404214720.0,
+    "gaze_score": 27.4166318429
+  },
+  {
+    "ProductionCosts": 115000000.0,
+    "BoxOffice": 474544677.0,
+    "gaze_score": 25.0
+  },
+  {
+    "ProductionCosts": 185000000.0,
+    "BoxOffice": 534987076.0,
+    "gaze_score": 25.0
+  },
+  {
+    "ProductionCosts": 800000.0,
+    "BoxOffice": 82000000.0,
+    "gaze_score": 35.963527441
+  },
+  {
+    "ProductionCosts": 55000000.0,
+    "BoxOffice": 330455270.0,
+    "gaze_score": 60.1795280197
+  },
+  {
+    "ProductionCosts": 250000000.0,
+    "BoxOffice": 302334374.0,
+    "gaze_score": 35.3051624441
+  },
+  {
+    "ProductionCosts": 225000000.0,
+    "BoxOffice": 423315812.0,
+    "gaze_score": 41.5382130082
+  },
+  {
+    "ProductionCosts": 35000000.0,
+    "BoxOffice": 251409241.0,
+    "gaze_score": 56.9182532386
+  },
+  {
+    "ProductionCosts": 30000000.0,
+    "BoxOffice": 243578797.0,
+    "gaze_score": 38.33
+  },
+  {
+    "ProductionCosts": 15000000.0,
+    "BoxOffice": 180258178.0,
+    "gaze_score": 41.5382130082
+  },
+  {
+    "ProductionCosts": 10500000.0,
+    "BoxOffice": 60481243.0,
+    "gaze_score": 65.0
+  },
+  {
+    "ProductionCosts": 125000000.0,
+    "BoxOffice": 317557891.0,
+    "gaze_score": 25.0
+  },
+  {
+    "ProductionCosts": 19000000.0,
+    "BoxOffice": 212836762.0,
+    "gaze_score": 38.33
+  },
+  {
+    "ProductionCosts": 237000000.0,
+    "BoxOffice": 760507625.0,
+    "gaze_score": 33.0150334308
+  },
+  {
+    "ProductionCosts": 245000000.0,
+    "BoxOffice": 936662225.0,
+    "gaze_score": 30.9268700376
+  },
+  {
+    "ProductionCosts": 200000000.0,
+    "BoxOffice": 700426566.0,
+    "gaze_score": 31.9620784491
+  },
+  {
+    "ProductionCosts": 200000000.0,
+    "BoxOffice": 532177324.0,
+    "gaze_score": 25.0
+  },
+  {
+    "ProductionCosts": 200000000.0,
+    "BoxOffice": 620181382.0,
+    "gaze_score": 16.05
+  }
+]
 
 /* 
 var data = [{
